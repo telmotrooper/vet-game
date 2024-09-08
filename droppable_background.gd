6c51a98e581f8caeb279b3a7962c0dcbc2ca51fa
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-08 14:16:25 -0300

    can drop object anywhere

---
+++ droppable_background.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
+	return true
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	data.position = at_position
