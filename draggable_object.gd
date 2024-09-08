18d69381e2d16d3bc66c47aad0be9c198967f7a4
Telmo "Trooper <telmo.valverde@tarmac.io>
2024-09-08 14:27:03 -0300

    initial prototype (#1)
    * setting up basic structure
    
    * drag and drop
    
    * added sprite for slot
    
    * fixed error when changing scenes
    
    * able to drag and drop object into slot
    
    * can drop object anywhere
    
    * hide parent node when moving

---
+++ draggable_object.gd
@@ -0,0 +1,21 @@
+class_name DraggableObject
+extends TextureRect
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	self_modulate.a = 0 # Hide node, but not its children.
+	
+	var drag_preview = Control.new()
+	
+	var child: TextureRect = TextureRect.new()
+	child.texture = texture
+
+	# TODO
+	drag_preview.add_child(child)
+	
+	set_drag_preview(drag_preview)
+	
+	return self
+
+# This is called when the object is dropped.
+func _on_item_rect_changed() -> void:
+	self_modulate.a = 1 # Display node again.
