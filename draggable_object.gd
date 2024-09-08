c48657ad82d4a3441eacc793c9b37e3b617c00a7
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-08 15:16:31 -0300

    position preview at the center of the mouse

--- draggable_object.gd
+++ draggable_object.gd
@@ -8,12 +8,13 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	
 	var child: TextureRect = TextureRect.new()
 	child.texture = texture
-
-	# TODO
 	drag_preview.add_child(child)
 	
-	set_drag_preview(drag_preview)
+	# Position preview at the center of the mouse.
+	var texture_size = child.texture.get_size()
+	child.position = Vector2(-texture_size.x / 2, -texture_size.y / 2)
 	
+	set_drag_preview(drag_preview)
 	return self
 
 # This is called when the object is dropped in a different position.
