c21b9848b00a370154693784a23a9ac33e125ed4
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-08 14:24:44 -0300

    hide parent node when moving

--- draggable_object.gd
+++ draggable_object.gd
@@ -2,6 +2,8 @@ class_name DraggableObject
 extends TextureRect
 
 func _get_drag_data(_at_position: Vector2) -> Variant:
+	self_modulate.a = 0 # Hide node, but not its children.
+	
 	var drag_preview = Control.new()
 	
 	var child: TextureRect = TextureRect.new()
@@ -13,3 +15,7 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	set_drag_preview(drag_preview)
 	
 	return self
+
+# This is called when the object is dropped.
+func _on_item_rect_changed() -> void:
+	self_modulate.a = 1 # Display node again.
