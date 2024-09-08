569a740e739a3729a94d49795c757358625929d2
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-08 13:59:09 -0300

    fixed error when changing scenes

--- main.gd
+++ main.gd
@@ -3,4 +3,4 @@ extends Node2D
 @export var initial_scene: PackedScene
 
 func _ready() -> void:
-	get_tree().change_scene_to_packed(initial_scene)
+	get_tree().call_deferred("change_scene_to_packed", initial_scene)
