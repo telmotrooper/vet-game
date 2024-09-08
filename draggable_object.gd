5b0bfcaee5fc6132386cde36efd03ef3972631e5
Telmo "Trooper <telmo.valverde@tarmac.io>
2024-09-08 15:49:52 -0300

    compensate mouse position when dragging (#2)
    * added background music
    
    * improved comment on function
    
    * position preview at the center of the mouse
    
    * drag from mouse position
    
    * use correct position when dropping object onto the background

--- draggable_object.gd
+++ draggable_object.gd
@@ -1,21 +1,24 @@
 class_name DraggableObject
 extends TextureRect
 
-func _get_drag_data(_at_position: Vector2) -> Variant:
+var drag_position: Vector2
+
+func _get_drag_data(at_position: Vector2) -> Variant:
 	self_modulate.a = 0 # Hide node, but not its children.
 	
 	var drag_preview = Control.new()
 	
 	var child: TextureRect = TextureRect.new()
 	child.texture = texture
-
-	# TODO
 	drag_preview.add_child(child)
 	
-	set_drag_preview(drag_preview)
+	# Compensate mouse position.
+	child.position = Vector2(-at_position.x, -at_position.y)
+	drag_position = child.position
 	
+	set_drag_preview(drag_preview)
 	return self
 
-# This is called when the object is dropped.
+# This is called when the object is dropped in a different position.
 func _on_item_rect_changed() -> void:
 	self_modulate.a = 1 # Display node again.
--- slot.gd
+++ slot.gd
@@ -1,9 +1,7 @@
 extends TextureRect
 
 func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
-	print("_can_drop_data")
 	return true
 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	data.position = position
-	print(data)
