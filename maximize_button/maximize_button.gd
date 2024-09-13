8fe9f3a62503fa6d3f61d2b6d775ac3d9df2afed
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-13 17:22:47 -0300

    fixed bug regarding maximize icon

--- maximize_button/maximize_button.gd
+++ maximize_button/maximize_button.gd
@@ -4,17 +4,22 @@ extends Button
 @export var maximize_icon: Texture2D
 @export var minimize_icon: Texture2D
 
+# We store the window mode because on "web" the value of "get_window().mode" is not reliable.
+var window_mode: Window.Mode
+
 func _ready() -> void:
+	window_mode = get_window().mode
 	update_icon()
 
 func update_icon() -> void:
-	if get_window().mode == Window.MODE_FULLSCREEN:
+	if window_mode == Window.MODE_FULLSCREEN:
 		set_button_icon(minimize_icon)
 	else:
 		set_button_icon(maximize_icon)
 
 func toggle_fullscreen() -> void:
-	get_window().mode = Window.MODE_FULLSCREEN if get_window().mode != Window.MODE_FULLSCREEN else Window.MODE_WINDOWED
+	window_mode = Window.MODE_FULLSCREEN if get_window().mode != Window.MODE_FULLSCREEN else Window.MODE_WINDOWED
+	get_window().mode = window_mode
 
 func _on_pressed() -> void:
 	toggle_fullscreen()
