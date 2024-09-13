2ef0ed47fa51177ee97e4aae93e232d0dfd13fd4
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-13 13:38:35 -0300

    added a couple useful comments

--- droppable_background.gd
+++ droppable_background.gd
@@ -4,4 +4,5 @@ func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
 	return true
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
+	# Allow dropping the object anywhere in the background.
 	data.position = at_position + data.drag_position
