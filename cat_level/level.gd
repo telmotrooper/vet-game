006e115db58773817576fa2a00ba15bcde662ceb
Telmo Valverde <telmo.valverde@tarmac.io>
2024-09-28 19:32:13 -0300

    added score counter

---
+++ cat_level/level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var slot_counter: int
+var score := 0
+
+func _ready() -> void:
+	for child in get_children():
+		if child is Slot:
+			slot_counter += 1
+	
+	%Score.text = "[b]Acertos:[/b] %d/%d" % [score, slot_counter]
+
+func _on_slot_filled() -> void:
+	score += 1
+	%Score.text = "[b]Acertos:[/b] %d/%d" % [score, slot_counter]
