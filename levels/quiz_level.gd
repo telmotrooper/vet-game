7335981f92ab543548dac6541dbd80ab75cfa784
Telmo "Trooper <telmo.trooper@gmail.com>
2024-11-03 14:25:07 -0300

    initial work on quiz level (#29)
    * beginning work on quiz level
    
    * adapted 'cat level' to match standard
    
    * working on quiz
    
    * organizing things

---
+++ levels/quiz_level.gd
@@ -0,0 +1,36 @@
+extends Control
+
+var question_counter: int
+var score := 0
+var mistakes := 0
+
+const base_text := "[center][b]( Acertos:[/b] %d/%d )[/center]"
+
+func _ready() -> void:
+	%VictoryPanel.visible = false
+	%VictoryPanel.scale = Vector2.ZERO
+	for child in %Questions.get_children():
+		question_counter += 1
+	update_text()
+
+func _on_slot_filled() -> void:
+	score += 1
+	update_text()
+	if score == question_counter:
+		show_victory_panel()
+
+func update_text() -> void:
+	%Score.text = base_text % [score, question_counter]
+
+func show_victory_panel() -> void:
+	if mistakes == 0:
+		%VictoryPanel.set_stars(3)
+	elif mistakes <= 3:
+		%VictoryPanel.set_stars(2)
+	else:
+		%VictoryPanel.set_stars(1)
+	
+	%VictoryPanel.show_panel()
+
+func _on_mistake_made():
+	mistakes += 1
--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -1,7 +1,10 @@
 extends Control
 
 func _on_dog_button_pressed() -> void:
-	GameState.load_scene("res://dog_level/dog_level.tscn")
+	GameState.load_scene("res://levels/dog_level/dog_level.tscn")
 
 func _on_cat_button_pressed() -> void:
-	GameState.load_scene("res://cat_level/cat_level.tscn")
+	GameState.load_scene("res://levels/cat_level/cat_level.tscn")
+
+func _on_lympathic_system_button_pressed() -> void:
+	GameState.load_scene("res://levels/lymphatic_system_level/lymphatic_system_level.tscn")
