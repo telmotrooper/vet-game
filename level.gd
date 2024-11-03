b2a2d7ef238512bbd2b76fadd02028d5740ec5d3
Telmo "Trooper <telmo.trooper@gmail.com>
2024-11-03 13:30:18 -0300

    moved 'victory panel' to its own scene (#28)
    * working on top buttons
    
    * moved 'victory panel' to its own scene

--- level.gd
+++ level.gd
@@ -1,8 +1,5 @@
 extends Control
 
-@export var empty_star: Texture2D
-@export var filled_star: Texture2D
-
 var slot_counter: int
 var score := 0
 var mistakes := 0
@@ -26,35 +23,15 @@ func _on_slot_filled() -> void:
 func update_text() -> void:
 	%Score.text = base_text % [score, slot_counter]
 
-func set_stars(quantity: int):
-	%Stars/Star.texture = filled_star if quantity >= 1 else empty_star
-	%Stars/Star2.texture = filled_star if quantity >= 2 else empty_star
-	%Stars/Star3.texture = filled_star if quantity >= 3 else empty_star
-
 func show_victory_panel() -> void:
 	if mistakes == 0:
-		set_stars(3)
+		%VictoryPanel.set_stars(3)
 	elif mistakes <= 3:
-		set_stars(2)
+		%VictoryPanel.set_stars(2)
 	else:
-		set_stars(1)
+		%VictoryPanel.set_stars(1)
 	
-	for star in %Stars.get_children():
-		star.custom_minimum_size = Vector2.ZERO
-
-	%VictoryPanel.set_visible(true)
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(%VictoryPanel, "scale", Vector2(1,1), 0.7)
-	tween.tween_property(%Stars/Star, "custom_minimum_size", Vector2(128,128), 0.3)
-	tween.tween_property(%Stars/Star2, "custom_minimum_size", Vector2(128,128), 0.3)
-	tween.tween_property(%Stars/Star3, "custom_minimum_size", Vector2(128,128), 0.3)
-
-func hide_victory_panel() -> void:
-	var tween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(%VictoryPanel, "scale", Vector2(0,0), 0.7)
-	tween.tween_callback(%VictoryPanel.hide)
+	%VictoryPanel.show_panel()
 
 func _on_mistake_made():
 	mistakes += 1
