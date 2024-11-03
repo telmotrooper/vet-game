9ab3d2cd6e6da5e63d5b8d2fc37181128eeae9a1
Telmo Valverde <telmo.valverde@tarmac.io>
2024-11-03 13:47:26 -0300

    beginning work on quiz level

--- title_screen/title_screen.gd
+++ title_screen/title_screen.gd
@@ -5,3 +5,6 @@ func _on_dog_button_pressed() -> void:
 
 func _on_cat_button_pressed() -> void:
 	GameState.load_scene("res://cat_level/cat_level.tscn")
+
+func _on_lympathic_system_button_pressed() -> void:
+	GameState.load_scene("res://lymphatic_system_level/lymphatic_system_level.tscn")
