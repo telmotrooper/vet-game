5f8b071858bd7fd8e5fbf617ad006be3960b3159
Telmo Valverde <telmo.valverde@tarmac.io>
2024-11-06 23:03:15 -0300

    working on quiz #2

--- levels/quiz_level.gd
+++ levels/quiz_level.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var questions: Array[QuizQuestion]
 
+var current_question: QuizQuestion
 var question_counter: int
 var score := 0
 var mistakes := 0
@@ -9,17 +10,13 @@ var mistakes := 0
 const base_text := "[center][b]( Acertos:[/b] %d/%d )[/center]"
 
 func _ready() -> void:
+	current_question = questions.pick_random()
+	%Question.text = current_question.text
+	
 	%VictoryPanel.visible = false
 	%VictoryPanel.scale = Vector2.ZERO
-	for child in %Questions.get_children():
-		question_counter += 1
+	
 	update_text()
-
-func _on_slot_filled() -> void:
-	score += 1
-	update_text()
-	if score == question_counter:
-		show_victory_panel()
 
 func update_text() -> void:
 	%Score.text = base_text % [score, question_counter]
