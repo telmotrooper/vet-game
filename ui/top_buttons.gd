6b21d9a72c60fc5da1cd270feef82334f864ebb6
Telmo "Trooper <telmo.trooper@gmail.com>
2024-11-12 20:10:51 -0300

    added 'level selection' and 'credits' screens (#39)
    * added 'level selection' and 'credits' screens
    
    * fixed path bug
    
    * moved some assets to their own folder
    
    * added content to 'credits' scene

--- ui/top_buttons.gd
+++ ui/top_buttons.gd
@@ -2,7 +2,10 @@
 extends Control
 
 @export var hide_back_button: bool
+@export var back_button_target: String
 
 func _ready() -> void:
 	if hide_back_button:
 		$BackButton.set_visible(false)
+	if back_button_target:
+		$BackButton.target_scene = back_button_target
