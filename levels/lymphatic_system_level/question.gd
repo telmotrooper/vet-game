3ebf105a139f6525a5cbaa2fd91f54ee4217b443
Telmo Valverde <telmo.valverde@tarmac.io>
2024-11-15 16:31:39 -0300

    build 20241115-193133

--- levels/lymphatic_system_level/question.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name QuizQuestion
-
-@export var text: String = ""
-@export var correct_answer: String = ""
-@export var wrong_answers: Array[String]
--- title_screen/menu_button.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-@export var target_scene: String
-
-func _pressed() -> void:
-	GameState.load_scene(target_scene)
